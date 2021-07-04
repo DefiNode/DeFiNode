6df444bcd780f4b54f8c9b8888459be74e1cc98b
crypto-r <81986669+crypto-r@users.noreply.github.com>
2021-07-04 15:56:24 +0200

    Add files via upload

--- 02_download_snapshot.sh
+++ 02_download_snapshot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+start=$(date +%s)
+
 sudo curl -L https://raw.githubusercontent.com/DeFiCh/app/master/typings/snapshot.ts -O
 ADDRESS="$(cat snapshot.ts)| sed ':M;N;$!bM;s#\n# #g'"
 ADDRESS=$(echo $ADDRESS | sed "s|.*SNAPSHOT_EU\(.*\)SNAPSHOT_INDEX.*|\\1|"| awk -F\' '{print $2}')
@@ -11,3 +14,20 @@ mkdir defichain
 cd defichain
 sudo curl -L $ADDRESS$SNAPSHOT > snapshot.zip
 sudo unzip snapshot.zip
+
+end=$(date +%s)
+
+echo "elapsed time:" $((end-$start)) " seconds"
+
+hold=' '
+echo "Press 'SPACE' to return to menu"
+tty_state=$(stty -g)
+stty -icanon
+until [ -z "${hold#$in}" ] ; do
+    in=$(dd bs=1 count=1 </dev/tty 2>/dev/null)
+done
+stty "$tty_state"
+
+cd ..
+sudo sh 00_definode.sh
+
--- 03_build_images.sh
+++ 03_build_images.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
+
+defiChainStart=$(date +%s)
 cd images/defi_chain
 docker build . -t defi_chain | while read line ; do echo "$(date)| $line"; done;
-cd ../defi_wallet
+defiChainEnd=$(date +%s)
+
+defiWalletStart=$(date +%s)
+ cd ../defi_wallet
 docker build . -t defi_wallet | while read line ; do echo "$(date)| $line"; done;
+defiWalletEnd=$(date +%s)
+
+echo "elapsed build time DeFi Chain:" $((defiChainEnd-$defiChainStart)) " seconds"
+echo "elapsed build time DeFi Wallet:" $((defiWalletEnd-$defiWalletStart)) " seconds"
+
+hold=' '
+echo "Press 'SPACE' to return to menu"
+tty_state=$(stty -g)
+stty -icanon
+until [ -z "${hold#$in}" ] ; do
+    in=$(dd bs=1 count=1 </dev/tty 2>/dev/null)
+done
+stty "$tty_state"
+
+cd ../../
+sudo sh 00_definode.sh
