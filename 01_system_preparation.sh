739f5bc67ead90b0b987e305e8caf9d5ba03d43d
crypto-r <81986669+crypto-r@users.noreply.github.com>
2021-07-04 16:06:46 +0200

    Add files via upload

--- 01_system_preparation.sh
+++ 01_system_preparation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update -y && sudo apt-get upgrade -y
 mkdir docker
 cd docker
 curl -fsSL https://get.docker.com -o get-docker.sh
