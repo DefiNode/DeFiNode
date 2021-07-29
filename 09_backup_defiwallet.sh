344e07052291821d984d70ba584131158fb75699
crypto-r <81986669+crypto-r@users.noreply.github.com>
2021-07-29 19:24:20 +0200

    Add files via upload

--- 09_backup_defiwallet.sh
+++ 09_backup_defiwallet.sh
@@ -4,11 +4,15 @@
 
 BACKUP_WALLET_NAME=$(date +%F)
 
+mkdir $DEFI_HOME"/backup"
+
 cp $DEFI_HOME"/defichain/wallet.dat" $DEFI_HOME"/backup/"$BACKUP_WALLET_NAME"_wallet.dat"
+sudo chown pi $DEFI_HOME"/backup/"$BACKUP_WALLET_NAME"_wallet.dat"
+
 echo ""
 echo ""
 echo "********************************************************************************"
 echo "* Please Check your backup at" $DEFI_HOME"/backup/"$BACKUP_WALLET_NAME"_wallet.dat"
 echo "*"
-echo "* The node must be started again with '4 Run DeFi node'" 
+#echo "* The node must be started again with '4 Run DeFi node'" 
 echo "********************************************************************************"
