d123ff7c3ffd4e9d6a7daed0315a6e530c4bf46e
crypto-r <81986669+crypto-r@users.noreply.github.com>
2022-01-13 06:28:28 +0100

    Update 04_run_definode.sh
    Update for Raspi OS bullseye

--- 04_run_definode.sh
+++ 04_run_definode.sh
@@ -13,7 +13,7 @@ if [ -z ${DEFI_HOME+x} ]; then
 	echo ""
 else
 	FOLDER=$DEFI_HOME"/defichain"
-	docker run -d --restart always --name defi_node -p 8554:8554 -p 8555:8555 -p 5000:5000 -p 5001:5001 -v "$FOLDER:/root/.defi" defi_chain defid -maxmempool=64 -dbcache=128 -rpcallowip=172.17.0.0/16 -rpcbind=0.0.0.0 -rpcuser='definode_user' -rpcpassword='definode_pw'
+	docker run -d --restart always --name defi_node -p 8554:8554 -p 8555:8555 -p 5002:5000 -p 5001:5001 -v "$FOLDER:/root/.defi" defi_chain defid -maxmempool=64 -dbcache=128 -rpcallowip=172.17.0.0/16 -rpcbind=0.0.0.0 -rpcuser='definode_user' -rpcpassword='definode_pw'
 	docker run -d --restart always --network=container:defi_node --name defi_wallet -e RPC_HOST=127.0.0.1:8554 -e RPC_AUTH='definode_user:definode_pw' defi_wallet
 
 	cd $DEFI_HOME"/desktop"
