5421e53213fe13db661512c29ec7c6fd3e06163b
DJ4Blockchain <87339173+DJ4Blockchain@users.noreply.github.com>
2022-01-14 08:49:12 +0100

    docker port web changed

--- 01_system_preparation.sh
+++ 01_system_preparation.sh
@@ -7,15 +7,6 @@ sudo sh 10_init_defihome.sh
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install docker.io docker-compose rootlesskit docker-registry -y
 
-#mkdir docker
-#cd docker
-#curl -fsSL https://get.docker.com -o get-docker.sh
-#sudo sh get-docker.sh
-#sudo usermod -aG docker pi
-
-#wget http://ftp.de.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.1-1_armhf.deb
-#sudo dpkg -i libseccomp2_2.5.1-1_armhf.deb
-
 ## Setup Shortcuts
 cd ../desktop
 cp ./defiChainLogo1.png /usr/share/pixmaps
