49d1e9472f366e3769e97f9afac7f78d968ff933
DJ4Blockchain <87339173+DJ4Blockchain@users.noreply.github.com>
2022-01-15 19:47:23 +0100

    change setup autostart

--- 01_system_preparation.sh
+++ 01_system_preparation.sh
@@ -9,9 +9,9 @@ sudo apt install docker.io docker-compose rootlesskit docker-registry -y
 
 ## Setup Shortcuts
 cd ../desktop
-cp ./defiChainLogo1.png /usr/share/pixmaps
-cp ./defiChainLogo3.png /usr/share/pixmaps
-cp ./defiNodeMenuShortcut.desktop /home/pi/Desktop
+sudo cp ./defiChainLogo1.png /usr/share/pixmaps
+sudo cp ./defiChainLogo3.png /usr/share/pixmaps
+sudo cp ./defiNodeMenuShortcut.desktop /home/pi/Desktop
 
 cd ../util
-cp ./startMainMenu.sh /home/pi
+sudo cp ./startMainMenu.sh /home/pi
--- util/setupAutostart.sh
+++ util/setupAutostart.sh
@@ -4,5 +4,5 @@
 if [ ! -d /home/pi/.config/autostart ]; then
 	mkdir /home/pi/.config/autostart
 fi
-cp ./autostart/defiAutostart.desktop /home/pi/.config/autostart
-cp ./autostart/check-DeFiWallet.sh /home/pi
+sudo cp ./autostart/defiAutostart.desktop /home/pi/.config/autostart
+sudo cp ./autostart/check-DeFiWallet.sh /home/pi
